2acd33eae05fd51ffc3fddea1d08709574273cef
jshrewsbury <jshrewsbury23@gmail.com>
2015-11-17 14:13:22 -0500

    Update prisoners_dilemma.py
    Hey just adding comments

--- PrisonersDilemma/PrisonersDilemma/prisoners_dilemma.py
+++ PrisonersDilemma/PrisonersDilemma/prisoners_dilemma.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 '''
+some changes made
 PrisonerDilemma.py allows hard-coding different strategies
 for the Iterative Prisoners Dilemma, the canonical game of game-theory.
 Each strategy plays 100 to 200 rounds against each other strategy.
